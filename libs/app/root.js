af345ef3f1034ef5d055980e59f1f35a987cbe86
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-02-27 17:54:23 +0530

    first commit

--- libs/app/root.js
+++ libs/app/root.js
@@ -1,12 +1,10 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
 function apphandle(req, res,path,port,os,fs,envvar){
     a=req.params[0].split('/')
     if(a[1]=='ua'){
         res.send(DeviceDetector.DeviceDetectorAPI(req.get('User-Agent')));
     } else{
-        res.send('Hello '+a[1]);
+        res.header("Content-Type", "text/html");
+        res.render('index.html');
     }
 }
 module.exports={
