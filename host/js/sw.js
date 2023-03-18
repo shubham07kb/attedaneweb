a128d84f262e8adbfbb44991abb53c5ec6a96e5a
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-03-18 14:05:32 +0530

    first commit

---
+++ host/js/sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/styles.css',
+                '/main.js'
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
