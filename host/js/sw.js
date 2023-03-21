dac61bca91a6cdc384d8dd5f4bd2fc3195f1f40b
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-03-21 19:55:45 +0530

    first commit

--- host/js/sw.js
+++ host/js/sw.js
@@ -1,30 +1,33 @@
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function (cache) {
-            return cache.addAll([
-                '/',
-                '/app.js',
-                '/favicon.ico'
-            ]);
-        })
+const cacheName = 'v1';
+const cacheAssets = [
+    '/',
+    '/app.js',
+    '/favicon.ico'
+];
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache=>{
+                cache.addAll(cacheAssets);
+            })
+            .then(() => { self.skipWaiting(); console.log('installed') })
     );
 });
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
+self.addEventListener('activate', e=>{
+    e.waitUntil(
+        caches.keys().then(cacheNames=>{
             return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
+                cacheNames.map(cache => {
+                    if (cache != cacheName) {
+                        console.log('Service Worker Clearing Cache');
+                        return caches.delete(cache);
+                    }
                 })
             );
         })
     );
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request).catch(()=>cacheAssets.match(e.request)));
 });
