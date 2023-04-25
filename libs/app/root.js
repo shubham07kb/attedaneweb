08308eb24d75c550cd238494b794a2ca1fcc732a
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-04-25 19:59:30 +0530

    first commit

--- libs/app/root.js
+++ libs/app/root.js
@@ -40,7 +40,7 @@ async function apphandle(req, res, path, port, os, fs, env) {
         var ptime = new Date();
         ptime = new Date(ptime.getTime() + (330 + ptime.getTimezoneOffset()) * 60000);
         const formatMap = {mm: ptime.getMonth() + 1, dd: ptime.getDate(), yyyy: ptime.getFullYear(), h: ptime.getHours(), m: ptime.getMinutes()};
-        p+='cltime="' + JSON.stringify(formatMap) + '";';
+        p+='cltime=`' + JSON.stringify(formatMap) + '`;';
         p+='siteurl="'+env.siteurl+'";';
         p+='isssl="'+env.isssl+'";';
         p+='webdarkcss=`'+fs.readFileSync(env.rootpath+'/host/css/webdark.css')+'`;';
