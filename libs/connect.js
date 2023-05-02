f24a3f90a9c1ec8cd788cbdf89465ba6c089bdb8
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2023-05-02 09:28:22 +0530

    first commit

--- libs/connect.js
+++ libs/connect.js
@@ -50,13 +50,13 @@ async function cron(env,req,res) {
         await db.update({ $set: { atten:  ctrps } }, { branch: ttdb[loop1].branch }, 'stuattenactive', env);
         await db.update({ $push: { atten: { $each: ctrpt } } }, { branch: ttdb[loop1].branch }, 'stuattenintive', env);
         var ptime = new Date();
-        ptime = new Date(ptime.getTime() + (330 + ptime.getTimezoneOffset()) * 60000);
-        formatMap = { mm: ptime.getMonth() + 1, dd: ptime.getDate(), yyyy: ptime.getFullYear(), d: ptime.getDay(), h: ptime.getHours(), m: ptime.getMinutes() };
-        fMmm = formatMap.mm.toString(), fMdd = formatMap.dd.toString(), fMyyyy = formatMap.yyyy.toString(), fMd = formatMap.d.toString(), fMh = formatMap.h.toString(), fMm = formatMap.m.toString(), 1 == fMmm.length && (fMmm = "0" + fMmm), 1 == fMdd.length && (fMdd = "0" + fMdd);
-        cr2 = fMmm + fMdd + fMyyyy + fMd + fMh + fMm;
         cronp.push({ branchc: ttdb[loop1].branch, intive: intive1, active: active1 });
     }
     if (cronstat == 1) {
+        ptime = new Date(ptime.getTime() + (330 + ptime.getTimezoneOffset()) * 60000);
+        formatMap = { mm: ptime.getMonth() + 1, dd: ptime.getDate(), yyyy: ptime.getFullYear(), d: ptime.getDay(), h: ptime.getHours(), m: ptime.getMinutes() };
+        fMmm = formatMap.mm.toString(), fMdd = formatMap.dd.toString(), fMyyyy = formatMap.yyyy.toString(), fMd = formatMap.d.toString(), fMh = formatMap.h.toString(), fMm = formatMap.m.toString(), 1 == fMmm.length && (fMmm = "0" + fMmm), 1 == fMdd.length && (fMdd = "0" + fMdd), 1 == fMh.length && (fMh = "0" + fMh), 1 == fMm.length && (fMm = "0" + fMm);
+        cr2 = fMmm + fMdd + fMyyyy + fMd + fMh + fMm;
         await db.update({ $set: { e: cr2, cronrun:cronp } }, { n: cr1 }, 'cronrecord', env);
     }
     res.send('{cronstat:"' + req.query.pass + ' accepted"}');
